477f566e2cc04e4c1b250b58579eea538efbf652
SANJA67 <615177v@mail.ru>
2022-03-18 00:33:08 +0300

    almost finished version

--- src/java/ru/javarush/cryptocode/cryptography/Crypto.java
+++ src/java/ru/javarush/cryptocode/cryptography/Crypto.java
@@ -16,8 +16,7 @@ public class Crypto {
     public List<String> encrypts(List<String> lineBuffer, boolean flagToSelectAction) {
         List<String> copyLineBuffer = new ArrayList<>(lineBuffer);
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter shift step. It must be a number between two and five.");
-        System.out.println(ALPHABET.length);
+        System.out.println("Enter the encryption key.");
         int key = 0;
 
         try {
@@ -37,12 +36,9 @@ public class Crypto {
                 if (checks(line.charAt(i))) {
                     if (Character.isUpperCase(line.charAt(i))) {
                         encryptedLine[i] =
-                                Character.toUpperCase(ALPHABET[determineEncodingIndex(line.charAt(i),
-                                        key, flagToSelectAction, uppercase)]);
+                                Character.toUpperCase(ALPHABET[determineEncodingIndex(key, flagToSelectAction, uppercase)]);
                     } else {
-                        encryptedLine[i] =
-                                ALPHABET[determineEncodingIndex(line.charAt(i),
-                                        key, flagToSelectAction, lowerCase)];
+                        encryptedLine[i] = ALPHABET[determineEncodingIndex(key, flagToSelectAction, lowerCase)];
                     }
                 } else {
                     encryptedLine[i] = line.charAt(i);
@@ -54,14 +50,12 @@ public class Crypto {
 
     }
 
-    private int determineEncodingIndex(char charAt, int key, boolean flagToSelectAction, boolean flag) {
-        System.out.println("Символ " + charAt);
-        System.out.println("Ключ " + key);
-        System.out.println("Флаг " + flagToSelectAction);
-        System.out.println("Номер буквы " + characterNumber);
+    private int determineEncodingIndex(int key, boolean flagToSelectAction, boolean flag) {
+
         if (!flagToSelectAction) {
             key = -1 * key;
         }
+
         int arrayLengthAdjustment = 1;
         int index = characterNumber + key;
         int noita = NUMBER_OF_CHARACTERS_IN_THE_ARRAY;
@@ -69,31 +63,38 @@ public class Crypto {
 
         if (flag) {
             int output1 = index % (ALPHABET.length - noita) - arrayLengthAdjustment;
-            System.out.println("Первый проход " + output1);
+
             if (output1 < 0) {
                 output1 = output1 + (ALPHABET.length - noita);
             }
+
             return output1;
 
         } else {
+
             int output2 = index % (ALPHABET.length) - arrayLengthAdjustment;
-            System.out.println("Второй проход " + output2);
+
             if (output2 < 0) {
                 output2 = output2 + (ALPHABET.length);
             }
+
             return output2;
         }
     }
 
     private boolean checks(Character symbol) {
+
         characterNumber = 0;
         symbol = Character.toLowerCase(symbol);
+
         for (Character characterFromArray : ALPHABET) {
             characterNumber++;
+
             if (characterFromArray.equals(symbol)) {
                 return true;
             }
         }
+
         return false;
     }
 }
